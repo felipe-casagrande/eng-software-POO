47bd06a3f14535024c19859093b03aca8d0b5a53
Felipe Casagrande <casagrandesaqua24@gmail.com>
2025-09-12 02:57:19 -0300

    adicionando codigo c#

---
+++ aula3/aula3/Pessoa.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aula3
+{
+    public class Pessoa
+    {
+        public string Nome { get; set; }
+        public string Sobrenome { get; set; }
+        public int Idade { get; set; }
+        public Pessoa(string nome, string sobrenome, int idade)
+        {
+            Nome = nome;
+            Sobrenome = sobrenome;
+            Idade = idade;
+        }
+    }
+}
---
+++ aula3/aula3/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aula3
+{
+    public class Programa
+    {
+        public static void Main(string[] args)
+        {
+
+            // criando nome
+
+            Console.WriteLine("Digite seu nome: ");
+            string nome_usuario = Console.ReadLine();
+
+            //criando sobrenome
+
+            Console.WriteLine("Digite seu sobrenome: ");
+            string sobrenome_usuario = Console.ReadLine();
+
+            //criando idade
+
+            Console.WriteLine("Digite sua idade: ");
+            string idade_texto = Console.ReadLine();
+            int idade_usuario = int.Parse(idade_texto);
+            //mudando nome
+            Console.WriteLine("Digite um novo nome");
+            nome_usuario = Console.ReadLine();
+            // criando a pessoa
+
+            Pessoa p1 = new Pessoa(nome_usuario,sobrenome_usuario,idade_usuario);
+            Console.WriteLine($"Pessoa 1: {p1.Nome} {p1.Sobrenome} tem {p1.Idade} anos!");
+        }
+    }
+          
+}
