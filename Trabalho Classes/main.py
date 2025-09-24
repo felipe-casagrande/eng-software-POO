b08540b17aa2cee8734929742ebb66e57414791b
Felipe Casagrande <casagrandesaqua24@gmail.com>
2025-09-24 17:05:02 -0300

    adicionando comentarios

--- Trabalho Classes/main.py
+++ Trabalho Classes/main.py
@@ -1,12 +1,23 @@
 from Pessoa import Pessoa, Objeto
 from Banco import Banco
 
+# Como usar os metodos:
+#parametros
+    # TED - informar o banco de destino e o valor 
+    # saque - apenas o valor
+    # Pix - Apenas o valor
+    # Cheque especial - Apenas o valor, liberado para contas negativadas ou com 0 reais
+    # Emprestimo - Apenas o valor, mas tera uma condição de apenas usuario com 21 anos realizar (fiz só pra ter algo para o perfil ser avaliado)
+    # Credito - Informar o que esta comprando ou loja e depois o valor. Exemplo: ("Celular",1000.00) ou ("Americanas",5.99) 
+    # Ver fatura - Só chamar a funçao e tera o relatorio das compras do cartao de credito
+
 pessoa1 = Pessoa('felipe',22)
 pessoa2 = Pessoa('Diego', 30)
 
 conta1 = Banco(21,pessoa1,100,"itau",2021)
 conta2 = Banco(22,pessoa2,10000,'Bradesco',2222)
 
+    
 
 conta1.saque(10)
 conta1.Pix(10)
