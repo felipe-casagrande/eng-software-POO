684b77c30578d1654db2bba862cf82c813124fba
Felipe Casagrande <casagrandesaqua24@gmail.com>
2025-10-01 22:23:02 -0300

    praticando

---
+++ Revisao2/classes.py
@@ -0,0 +1,212 @@
+
+class Chave:
+    def __init__(self, id_chave: str):
+        self.id_chave = id_chave
+
+
+class Carro:
+    def __init__(self, id_chave: str):
+        self.chave = id_chave
+        self.porta_aberta = False
+        self.ligado = False
+        self.velocidade = 0.0
+        self.marcha = 0
+        self.pessoa_dentro:'Pessoa' = None
+        self.historico = []
+
+    def abrir(self, chave):
+        if self.chave == chave:
+            self.porta_aberta = True
+            print('Abrindo carro')
+            self.historico.append('Abrir carro')    
+            
+        else:
+            print('Chave nao compativel ')
+
+    def fechar(self, chave):
+        if self.chave == chave:
+            self.porta_aberta = False
+            print('Fechando carro')
+            self.historico.append('Fechar carro')    
+        else:
+            print('Chave nao compativel para fechar')
+ 
+    def ligar(self, pessoa: "Pessoa"):
+        if self.chave == pessoa.chave.id_chave:
+            if not self.porta_aberta and pessoa.dentro_carro:
+                self.ligado = True
+                print(f'Ligando carro!')
+                self.historico.append('Ligar carro')
+            else:
+                print('algo esta errado, nao posso ligar')
+        else:
+            print('Chave Incorreta')
+    
+
+    def desligar(self, pessoa: "Pessoa"):
+        #nao esta no texto, mas irei colocar para apenas desligar se estiver parado, se nao a pessoa pode desligar o carro e sair dele c ele em movimento
+        if self.chave == pessoa.chave.id_chave:
+            if not self.porta_aberta and pessoa.dentro_carro and self.velocidade == 0.0:
+                self.ligado = False
+                print('desligando...')
+                self.historico.append('Desligar carro')
+            else:
+                print('Nao posso desligar')   
+
+        else:
+            print('Chave nao esta valida')        
+
+    def acelerar(self,pessoa: "Pessoa"):
+        #pelo que entendi, dependendo da marcha que estiver a aceleração vai ser diferente, entao vamos lá, irei fazer até a quinta marcha
+        # nao entendi a necessidade do pessoa.., entao irei colocar que apenas a pessoa que esta dentro pode acelerar ou frear
+        if self.chave == pessoa.chave.id_chave:
+            if self.pessoa_dentro is not pessoa:
+                print("Somente a pessoa autorizada dentro do carro pode acelerar.")
+            else:
+                if self.porta_aberta or not self.pessoa_dentro:
+                    print('Nao tem como acelerar com a porta aberta ou sem pessoas dentro...')
+                else:
+                    if self.ligado:
+                        if self.marcha == 1:
+                            self.velocidade += 5.0
+                            print('Acelerando 5km')
+                            self.historico.append('Acelerando carro')
+                        elif self.marcha == 2:
+                            self.velocidade += 10.0
+                            print('Acelerando 10km')
+                            self.historico.append('Acelerando carro')
+                        elif self.marcha == 3:
+                            self.velocidade += 15.0 
+                            print('Acelerando 15km')
+                            self.historico.append('Acelerando carro')
+                        elif self.marcha == 4:
+                            self.velocidade += 20.0
+                            print('Acelerando 20km')
+                            self.historico.append('Acelerando carro')
+                        elif self.marcha == 5:
+                            self.velocidade += 25.0
+                            self.historico.append('Acelerando carro')
+                            print('Acelerando 25km')
+                            self.historico.append('Acelerando carro')   
+                        elif self.marcha == 6:
+                            self.velocidade += 30.0
+                            print('Acelerando 30km')
+                            self.historico.append('Acelerando carro')
+                        else:
+                            print('Passe uma marcha primeiro para acelerar!')    
+                    else:
+                        print('Carro nao esta ligado para acelerar')
+        else:
+            print('Chave nao esta valida')                
+
+    def frear(self,pessoa:"Pessoa"):
+        if self.chave == pessoa.chave.id_chave:
+            if self.pessoa_dentro is not pessoa:
+                print("Somente a pessoa autorizada dentro do carro pode acelerar.")
+            else:
+
+                if self.ligado and self.velocidade>0:
+                    self.velocidade -=5
+                    print(f'Diminuindo.., velocidade atual : {self.velocidade}')
+                    self.historico.append('Freando carro')
+                else:
+                    print('Carro nao esta em movimento para ser freiado')
+        else:
+            print('Chave nao esta valida')
+
+    def trocar_marcha(self,marcha):
+        
+        if marcha <0:
+            print('Nao pode trocar marcha para valores negativos..')
+        elif marcha == 0:
+            print('Marcha trocada para o neutro 0')    
+        elif marcha >7:
+            print('Nao tem carro com essa quantidade de marcha, passe ate a sexta marcha!')
+        else:
+            if marcha < self.marcha:
+                #nao sei quanto diminuir, irei tirar 5km
+                self.velocidade -=5.0
+                print(f"Diminuindo velocidade com freio motor, velocidade atual {self.velocidade}km/h")
+            self.marcha = marcha
+            self.historico.append('Trocando de marcha')
+    
+
+    def calcular_aceleracao(self):
+        print('')
+
+    def permitir_saida(self,pessoa:"Pessoa"):
+        if self.chave == pessoa.chave.id_chave:
+            if not self.ligado and self.porta_aberta and self.pessoa_dentro == pessoa:  #só pode sair com o carro desligado, depois de abrir a porta e se a pessoa que esta fazendo o comando é a que esta no carro. 
+                pessoa.dentro_carro = False
+                print('Saida autorizada,saindo')
+                self.historico.append('Sair do carro')
+            else:
+                print('Saida nao autorizada, desligue o carro, abra a porta ou verifique se é a pessoa correta')
+        else:
+            print('Chave nao esta valida')
+
+    def esta_parado(self):
+        if self.velocidade == 0.0:
+            print('Carro esta parado')
+        else:
+            print('Carro nao esta parado')
+
+    def ver_historico(self,carro):
+        print(f'HISTORICO DE AÇÕES DO CARRO SOLICITADO')
+        print('-'*30)
+        
+        for c in self.historico:
+            print(f'Ação realizada: {c}')
+
+
+class Pessoa:
+    def __init__(self, nome, sobrenome, idade: int, altura: float, chave: Chave):
+        self.nome = nome
+        self.sobrenome = sobrenome
+        self.idade = idade
+        self.altura = altura
+        self.velocidade: float = 0.0
+        self.chave = chave
+        self.dentro_carro = False
+
+    def abrir_carro(self, carro: "Carro"):
+        if carro.porta_aberta:
+            print('Carro ja esta aberto!')
+        else:
+            if self.chave.id_chave == carro.chave:
+                carro.abrir(self.chave.id_chave)
+               
+
+    def fechar_carro(self, carro: "Carro"):
+        if not carro.porta_aberta:
+            print('Porta ja esta fechada!')
+        else:
+
+            if self.chave.id_chave == carro.chave:
+                carro.fechar(self.chave.id_chave)
+             
+
+    def entrar_carro(self,carro: 'Carro'):
+        if self.dentro_carro:
+            print('Voce ja esta dentro do carro!')
+        else:
+            if carro.porta_aberta == False:
+                print('Primeiro abra a porta para poder entrar!')
+            else:
+                self.dentro_carro = True
+                carro.pessoa_dentro = self
+                print("Entrando no carro!")
+
+
+    def sair_carro(self,carro: 'Carro'):
+        if carro.ligado:
+            print('Desligue o carro primeiro para sair!')
+        else:
+            carro.permitir_saida(self)
+            
+            
+
+    
+             
+
+
---
+++ Revisao2/main.py
@@ -0,0 +1,54 @@
+"""
+porque pessoa tem velocidade? r = Ainda nao sei
+nao entendi pq acelerar e frear tem o parametro pessoa, oq colocar pra elas fazerem? R = Coloquei pra verificar a pessoa que ta fazendo a ação
+calcular aceleração? R = Nao entendi
+troca de marcha irei colocar até 6 marchas e que o freio é de 5 em 5km e o freio motor tambem de 5 em 5km
+bloquiei a troca de marcha para numeros maiores que 6 ou menor que 0, pois 0 seria o neutro.
+usei type hynts para ajudar.
+inicialmente nao usei a chave em todos os metodos, mas fiquei confuso no todas as operações validam chave correta, entao irei colocar pra verificar a chave em todos os metodos do carro.
+
+"""
+from classes import Carro,Chave,Pessoa
+ch1 = Chave('aa')
+p1 = Pessoa('Felipe','Casagrande',20,1.87,ch1)
+carro1 = Carro('aa')
+
+#teste para abrir a porta 
+#carro1.porta_aberta = True
+#carro1.abrir(ch1.id_chave)
+#carro1.abrir(ch1.id_chave)
+#p1.abrir_carro(carro1)
+
+#fechar carro
+#carro1.fechar(ch1.id_chave)
+#1.fechar_carro(carro1)
+
+#ligando carro
+
+#carro1.ligar(p1)
+#carro1.desligar(p1)
+
+#entrando no carro
+#p1.entrar_carro(carro1)
+#p1.entrar_carro(carro1)
+p1.abrir_carro(carro1)
+p1.entrar_carro(carro1)
+p1.fechar_carro(carro1)
+carro1.ligar(p1)
+carro1.acelerar(p1)
+carro1.trocar_marcha(0)
+carro1.acelerar(p1)
+carro1.trocar_marcha(1)
+carro1.acelerar(p1)
+carro1.trocar_marcha(2)
+carro1.porta_aberta = False
+carro1.acelerar(p1)
+carro1.trocar_marcha(1)
+carro1.frear(p1)
+carro1.frear(p1)
+carro1.frear(p1)
+p1.sair_carro(carro1)
+carro1.desligar(p1)
+p1.abrir_carro(carro1)
+p1.sair_carro(carro1)
+carro1.ver_historico(carro1)
